b6217b0ff7a067aa9e34e6c835bbbafcd49d17f8
Luis Hernandez Ayala <luis.hernandez@wizeline.com>
2020-02-04 12:11:41 -0600

    Styled options and error emssage

--- src/components/Options.js
+++ src/components/Options.js
@@ -4,18 +4,23 @@ import Option from './Option';
 
 const Options = (props) => (
   <div>
-    <button
-      className="button button--link"
-      onClick={props.handleDeleteOptions}
-    >
-      Remove All
-    </button>
-    {props.options.length == 0 && <p>Please add an option</p>}
+    <div className="widget-header">
+      <h3 className="widget-header__title">Your Options</h3>
+      <button
+        className="button button--link"
+        onClick={props.handleDeleteOptions}
+      >
+        Remove All
+      </button>
+    </div>
+
+    {props.options.length == 0 && <p className="widget-message">Please add an option to get started!</p>}
     {
     props.options.map((value, index) => 
       <Option 
         key={index} 
         optionText={value} 
+        count={index+1}
         handleDeleteOptionSingular={props.handleDeleteOptionSingular}
       />
     )
